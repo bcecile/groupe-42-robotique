5402e7e5d97d2e6cf1bb5cc33a64e57785c17519
Zhoupinou <60732105+Zhoupinou@users.noreply.github.com>
2020-02-26 22:05:17 +0100

    Update Arene.py

--- Arene.py
+++ Arene.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from Obstacle import *
 from Robot import *
 class Arene:
     
@@ -23,26 +24,59 @@ class Arene:
     def getLargeur(self):
         return self.largeur
 
+    # Une case 1 représente un robot
     def placerRobot(self):
         x=self.robot.getx()
         y=self.robot.gety()
         self.matrice[x][y]=1
         
+    # Une case 2 représente un obstacle
+    def placerObstacle(self,x,y,taille):
+        j=0 #int
+        i=0 # int
+        for i in range(taille):
+            for j in range(taille):
+                if ((self.matrice[x+i][y+j]==1)or(self.matrice[x+i][y+j]==2)):
+                    print("Erreur, la case est occupee \n")
+                else:
+                    self.matrice[x+i][y+j]=2
 
 #Test
 #instanciation de la classe
+                
 robot = Robot(1,1,0)
 Arene = Arene(5,5,robot)
+
 #Utilisation de la methode
+
 bob = Arene.construction(Arene.longueur,Arene.largeur)
-#Presentation du tableau 
+
+#Presentation du tableau
+
 print(bob)
+
 #On place le Robot dans l'Arene
+
 Arene.placerRobot()
+
 #Nouvelle representation du tableau
+
 print(bob)
+
+#Placer Obstacle
+
+obstacle = Obstacle(2,3,2)
+Arene.placerObstacle(obstacle.getx(),obstacle.gety(),obstacle.gettaille())
+print(bob)
+
+obstacle2 = Obstacle(1,1,1)
+Arene.placerObstacle(obstacle2.getx(),obstacle2.gety(),obstacle2.gettaille())
+
 #On fait avancer le Robot
-robot.avancerRobot(2,bob)
+
+#robot.avancerRobot(2,bob)
+
 #Resultat du deplacement
-print(bob)
+
+#print(bob)
 
