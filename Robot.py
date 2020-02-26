7db9282776e439816642c1dcc51eb6a6243a76d7
Zhoupinou <60732105+Zhoupinou@users.noreply.github.com>
2020-02-26 23:14:59 +0100

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -12,35 +12,69 @@ class Robot:
   #Methode get()
   def getx(self):
       return self.x
-  
+
   def gety(self):
       return self.y
 
   def getangle(self):
       return self.angle
 
+  def direction(self):
+    if self.angle==0:
+      return "bas"
+    elif self.angle==180:
+      return "droite"
+    elif self.angle==270:
+      return "haut"
+    elif self.angle==90:
+      return "gauche"
+
   def avancerRobot(self, distance, tab):
-            # Si angle = 0, il avance vers la droite ( vue du dessus )
-        if self.angle==0:
-            tab[self.x][self.y]=0
-            self.x=self.x+1
-            tab[self.x][self.y]=1
-        # Si angle = 90, il avance vers le haut
-        elif self.angle==90:
-            tab[self.x][self.y]=0
-            self.y=self.y+1
-            tab[self.x][self.y]=1
-        # Si angle = 180, il avance vers la gauche
-        elif self.angle==180:
-            tab[self.x][self.y]=0
-            self.x=self.x-1
-            tab[self.x][self.y]=1
-        # Si angle = 270, il avance vers le bas
-        elif self.angle==270:
-            tab[self.x][self.y]=0
-            self.y=self.y-1
-            tab[self.x][self.y]=1
-            
+    # Si angle = 180, il avance vers la droite ( vue du dessus )
+    if (self.direction()=="droite"):
+      tab[self.y][self.x]=0
+      self.x=self.x+distance
+      tab[self.y][self.x]=1
+    # Si angle =270 , il avance vers le haut
+    elif (self.direction()=="haut"):
+      tab[self.y][self.x]=0
+      self.y=self.y+distance
+      tab[self.y][self.x]=1
+    # Si angle = 90, il avance vers la gauche
+    elif (self.direction()=="gauche"):
+      tab[self.y][self.x]=0
+      self.x=self.x-distance
+      tab[self.y][self.x]=1
+    # Si angle =0 , il avance vers le bas
+    elif (self.direction()=="bas"):
+      tab[self.y][self.x]=0
+      self.y=self.y-distance
+      tab[self.y][self.x]=1
+
+  def collision(self, distance, tab):
+    i=1
+    j=1
+    if (self.direction()=="droite"):
+      for j in range(distance+1):
+        if (tab[self.y][self.x+j]==2):
+          return True
+        
+    elif (self.direction()=="haut"):
+      for i in range(distance+1):
+        if (tab[self.y+i][self.x]==2):
+          return True
+        
+    elif (self.direction()=="gauche"):
+      for j in range(distance+1):
+        if (tab[self.y][self.x-i]==2):
+          return True
+        
+    elif (self.direction()=="bas"):     
+      for i in range(distance+1):
+        if (tab[self.y-j][self.x]==2):
+          return True
+    return False
+  
 #Test
 
-
+robot = Robot(0,0,0)
